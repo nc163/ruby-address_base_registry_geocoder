b71dc76699f682382ae12d7ebbe58254e13c2bf7
usagi <kou1992@gmail.com>
2025-06-14 14:21:49 +0900

    細々修正

--- spec/address_base_registory_geocoder/client_spec.rb
+++ spec/address_base_registory_geocoder/client_spec.rb
@@ -9,35 +9,15 @@ describe AddressBaseRegistoryGeocoder::Client do
 
 
   describe '#geocode' do
-    let(:address) { '東京都台東区上野公園７−７' }
-    let(:response_body) { { result: { lat: 35.6895, lng: 139.6917 } } }
+    let(:address) { '東京都　台東区　上野公園　７−７' }
 
-    # before do
-    #   stub_request(:get, "#{base_url}/geocode")
-    #     .with(query: { address: address }, headers: { 'Authorization' => "Bearer #{api_key}" })
-    #     .to_return(status: 200, body: response_body.to_json, headers: { 'Content-Type' => 'application/json' })
-    # end
-
-    it 'returns geocode response' do
+    it 'is geocode response' do
       response = client.geocode(address)
       expect(response).to be_a(AddressBaseRegistoryGeocoder::Responses::GeocodeResponse)
+      # response.outputが、address から変わってることだけ判定したい
+      expect(response.output).not_to eq(address)
     end
+
   end
 
-  # describe '#handle_error' do
-  #   let(:address) { '123 Test St' }
-
-  #   before do
-  #     stub_request(:get, "#{base_url}/geocode")
-  #       .with(query: { address: address }, headers: { 'Authorization' => "Bearer #{api_key}" })
-  #       .to_return(status: 400, body: { error: 'Bad Request' }.to_json, headers: { 'Content-Type' => 'application/json' })
-  #   end
-
-  #   it 'raises an error with status and body' do
-  #     expect { client.geocode(address) }.to raise_error(AddressBaseRegistoryGeocoder::ErrorHandler) do |error|
-  #       expect(error.status).to eq(400)
-  #       expect(error.body).to eq('Bad Request')
-  #     end
-  #   end
-  # end
 end
--- spec/address_base_registory_geocoder/responses_spec.rb
+++ spec/address_base_registory_geocoder/responses_spec.rb
@@ -8,7 +8,7 @@ describe AddressBaseRegistoryGeocoder::Responses do
     let(:response_body) do
       [
         {
-          "query"=>{"input"=>"東京都台東区上野公園７−７"}, 
+          "query"=>{"input"=>"東京都 台東区 上野公園 ７−７"}, 
           "result"=>{"output"=>"東京都台東区上野公園7-7", "others"=>[], "score"=>0.93, "match_level"=>"residential_detail", "coordinate_level"=>"residential_detail", "lat"=>35.714952981, "lon"=>139.775372778, "lg_code"=>"131067", "machiaza_id"=>"0031000", "rsdt_addr_flg"=>1, "blk_id"=>"007", "rsdt_id"=>"007", "rsdt2_id"=>nil, "prc_id"=>nil, "pref"=>"東京都", "county"=>nil, "city"=>"台東区", "ward"=>nil, "oaza_cho"=>"上野公園", "chome"=>nil, "koaza"=>nil, "blk_num"=>"7", "rsdt_num"=>"7", "rsdt_num2"=>nil, "prc_num1"=>nil, "prc_num2"=>nil, "prc_num3"=>nil}
         }
       ]
